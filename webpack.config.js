6e8fd606340a0f3e658907ded5fe72bc98ad3eeb
William Liang <pineappletwo1@outlook.com>
2024-07-20 06:29:48 +0000

    chore: Add webpack configuration for bundling JavaScript files

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  // Setting the mode to 'production' optimizes the bundle size
+  mode: 'production',
+  // Entry point of your application
+  entry: './index.js',
+  // Configuration for the output of the bundle
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: 'bundle.js', // Name of the output file
+  },
+  target: 'node', // Ensures that Webpack compiles code for Node.js environment
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Transpile all .js files
+        exclude: /node_modules/, // Except for node_modules
+        use: {
+          loader: 'babel-loader', // Using babel-loader for transpiling
+          options: {
+            presets: ['@babel/preset-env'], // Preset used for env setup
+          },
+        },
+      },
+    ],
+  },
+};
