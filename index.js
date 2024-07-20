8bfaacf0bc774f6830a077a1cfc5b8a9a7f51e66
William Liang <pineappletwo1@outlook.com>
2024-07-20 06:34:05 +0000

    chore: Update static file serving directory to 'public'

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the 'src' directory
-app.use(express.static(path.join(__dirname, 'src')));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
